71fe6c76d37156aeadc17586acd9f3ac44b4a51e
hanna-ponomarenko <a.pokhyl@whalergames.com>
2026-02-26 14:42:27 +0200

    Core: board data model created

---
+++ Testgame/Assets/Scripts/Core/PieceType.cs
@@ -0,0 +1,16 @@
+namespace Testgame.Match3
+{
+    public enum PieceType
+    {
+        Color1 = 1,
+        Color2 = 2,
+        Color3 = 3,
+        Color4 = 4,
+        Color5 = 5,
+        Color6 = 6,
+        Color7 = 7,
+        Color8 = 8,
+        Color9 = 9,
+        Color10 = 10
+    }
+}
